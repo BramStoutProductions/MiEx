78b3d787493bb60d0bc8d90b1ce00adf0b0c15a9
Michal Bereznicki <20019071+michal212345@users.noreply.github.com>
2024-11-24 17:21:29 +0000

    Removed deprecated variable shadow_method

--- extras/Blender/BlenderAddon/MiExImport.py
+++ extras/Blender/BlenderAddon/MiExImport.py
@@ -67,7 +67,6 @@ class setup_materials:
         mat.use_backface_culling = True
         if self.hasTransparency:
             self.mat.blend_method = 'BLEND'
-            self.mat.shadow_method = 'HASHED'
 
     def import_node(self,name,data):
         node = self.mat.node_tree.nodes.new(data['type'])
