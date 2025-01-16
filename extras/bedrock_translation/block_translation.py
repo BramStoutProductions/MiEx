ff31227cbf426d2f3b5ec6ecd12a9b97200ecca1
Bram Stout <128681226+BramStout@users.noreply.github.com>
2025-01-16 14:48:51 +0100

    Updated Bedrock Translation files

--- extras/bedrock_translation/block_translation.py
+++ extras/bedrock_translation/block_translation.py
@@ -48,6 +48,7 @@ class BlockDef:
     def __init__(self, dataStr : str) -> None:
         self.name : str = ""
         self.state : dict[str, str] = {}
+        self.defaultValues : set[str] = set()
 
         openBrackedIndex = dataStr.find("[")
         if openBrackedIndex < 0:
@@ -86,18 +87,80 @@ class BlockDef:
             res = res * 31 + hash(value)
         return res
 
-def parseBlockB2J(data : dict[str, str], outMapping : dict[str, dict[BlockDef, BlockDef]]):
+def parseBlockB2J(data : dict[str, str], defaultStates : dict[str, dict[str, str]], outMapping : dict[str, dict[BlockDef, BlockDef]]):
     for key, value in data.items():
         bedrockDef : BlockDef = BlockDef(key)
         javaDef : BlockDef = BlockDef(value)
+
+        # We want to keep track of which states are the default value.
+        # These get put into the optional condition.
+        if bedrockDef.name in defaultStates:
+            defaultState = defaultStates[bedrockDef.name]
+            for stateName, stateValue in defaultState.items():
+                if stateName not in bedrockDef.state:
+                    continue
+                if bedrockDef.state[stateName] == stateValue:
+                    bedrockDef.defaultValues.add(stateName)
+
         if bedrockDef.name not in outMapping:
             outMapping[bedrockDef.name] = {}
         outMapping[bedrockDef.name][bedrockDef] = javaDef
+
+def parseBlocks(data : list[dict[str, int]], outStateIds : dict[str, int]):
+    for blockData in data:
+        if "name" not in blockData:
+            continue
+        if "defaultState" not in blockData:
+            continue
+        blockName = blockData["name"]
+        blockState = blockData["defaultState"]
+        if ":" not in blockName:
+            blockName = "minecraft:" + blockName
+        outStateIds[blockName] = blockState
+
+def parseBlockStates(data : list[dict], stateIds : dict[str, int], outDefaultStates : dict[str, dict[str, str]]):
+    dataLength = len(data)
+    for blockName, stateId in stateIds.items():
+        if stateId < 0 or stateId >= dataLength:
+            continue
+        blockState : dict = data[stateId]
+        if "states" not in blockState:
+            continue
+
+        defaultState : dict[str, str] = {}
+
+        blockState : dict = blockState["states"]
+        for stateName, stateData in blockState.items():
+            if "value" not in stateData:
+                continue
+
+            defaultState[stateName] = str(stateData["value"])
+        
+        if blockName not in outDefaultStates:
+            outDefaultStates[blockName] = {}
+        outDefaultStates[blockName].update(defaultState)
 
 def run():
     if not os.path.exists(MINECRAFT_DATA_BEDROCK_DIR) or not os.path.isdir(MINECRAFT_DATA_BEDROCK_DIR):
         print("No valid minecraft-data/data/bedrock directory found")
         return
+    
+    defaultStates : dict[str, dict[str, str]] = {}
+    for dirName in os.listdir(MINECRAFT_DATA_BEDROCK_DIR):
+        dirPath = MINECRAFT_DATA_BEDROCK_DIR + "/" + dirName
+        blocksPath = dirPath + "/blocks.json"
+        if not os.path.exists(blocksPath):
+            continue
+        blockStatesPath = dirPath + "/blockStates.json"
+        if not os.path.exists(blockStatesPath):
+            continue
+
+        defaultStateIds : dict[str, int] = {}
+        with open(blocksPath, encoding="UTF-8") as fp:
+            parseBlocks(json.load(fp), defaultStateIds)
+        
+        with open(blockStatesPath, encoding="UTF-8") as fp:
+            parseBlockStates(json.load(fp), defaultStateIds, defaultStates)
 
     mapping : dict[str, dict[BlockDef, BlockDef]] = {}
 
@@ -111,7 +174,7 @@ def run():
             continue
 
         with open(blocksB2JPath, encoding='UTF-8') as fp:
-            parseBlockB2J(json.load(fp), mapping)
+            parseBlockB2J(json.load(fp), defaultStates, mapping)
     
     # Go through mapping to remove mappings that don't change anything
     toRemove = []
@@ -129,6 +192,28 @@ def run():
             toRemove.append(key)
     for key in toRemove:
         mapping.pop(key)
+    
+    # Go through mapping and apply some patches to fix certain things
+    for value in mapping.values():
+        for bedrockDef, javaDef in value.items():
+            if "stairs" in javaDef.name:
+                # Stairs by default get the shape "outer_right", but should have the shape "straight"
+                if "shape" in javaDef.state:
+                    javaDef.state["shape"] = "straight"
+            elif bedrockDef.name == "minecraft:trip_wire":
+                # We need to get rid of "suspended_bit" from condition
+                if "suspended_bit" in bedrockDef.state:
+                    bedrockDef.state.pop("suspended_bit")
+            elif bedrockDef.name == "minecraft:snow_layer":
+                # Remove covered_bit from condition
+                if "covered_bit" in bedrockDef.state:
+                    bedrockDef.state.pop("covered_bit")
+            elif bedrockDef.name == "minecraft:leaves":
+                # Remove update_bit and persistent_bit
+                if "update_bit" in bedrockDef.state:
+                    bedrockDef.state.pop("update_bit")
+                if "persistent_bit" in bedrockDef.state:
+                    bedrockDef.state.pop("persistent_bit")
 
     # Now we need to write the mapping file out into a json file
     translations : dict = {}
@@ -140,7 +225,19 @@ def run():
                 "name": javaDef.name
             }
             if len(bedrockDef.state) > 0:
-                map["condition"] = bedrockDef.state
+                if len(bedrockDef.defaultValues) == 0:
+                    map["condition"] = bedrockDef.state
+                else:
+                    # Some states are optional, so separate it out into an optional condition
+                    conditionMap : dict[str, str] = {}
+                    optionalConditionMap : dict[str, str] = {}
+                    for stateName, stateValue in bedrockDef.state.items():
+                        if stateName in bedrockDef.defaultValues:
+                            optionalConditionMap[stateName] = stateValue
+                        else:
+                            conditionMap[stateName] = stateValue
+                    map["condition"] = conditionMap
+                    map["optionalCondition"] = optionalConditionMap
             if len(javaDef.state) > 0:
                 map["constants"] = javaDef.state
             mappings.append(map)
